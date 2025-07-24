8f50a0cccb4bff003d31eaf194b5c9d4248b7b2a
Sheepo915 <shaoyang.chong@gmail.com>
2025-07-25 00:38:43 +0800

    Completed Masonry Feature

--- src/components/common/Masonry.tsx
+++ src/components/common/Masonry.tsx
@@ -4,25 +4,47 @@ import { twMerge } from "tailwind-merge";
 interface MasonryProps<T> extends HTMLProps<HTMLDivElement> {
   data: T[];
   colNumber: number;
+  orientation?: "vertical" | "horizontal";
   render: (item: T, index: number) => React.ReactNode;
 }
 
-export default function Masonry<T>({ data, colNumber, render, ...props }: MasonryProps<T>) {
+export default function Masonry<T>({
+  data,
+  colNumber,
+  orientation = "horizontal",
+  render,
+  ...props
+}: MasonryProps<T>) {
   const template = useMemo(() => {
     const t: Map<number, T[]> = new Map();
-    for (let i = 0; i < colNumber; i++) {
-      t.set(
-        i,
-        data.filter((_, index) => index % colNumber === i)
-      );
+    switch (orientation) {
+      case "horizontal": {
+        for (let i = 0; i < colNumber; i++) {
+          t.set(
+            i,
+            data.filter((_, index) => index % colNumber === i)
+          );
+        }
+        break;
+      }
+      case "vertical": {
+        let start = 0;
+        const chunkSize = Math.ceil(data.length / colNumber);
+        for (let i = 0; i < colNumber; i++) {
+          const end = start + chunkSize;
+          t.set(i, data.slice(start, end));
+          start = end;
+        }
+        break;
+      }
     }
     return t;
-  }, [colNumber, data]);
+  }, [colNumber, data, orientation]);
 
   return (
     <div
       {...props}
-      className={twMerge("grid grid-cols-2 md:grid-cols-3 gap-4 mt-20", props?.className)}
+      className={twMerge("grid grid-flow-rows grid-cols-2 md:grid-cols-3 gap-4", props?.className)}
     >
       {[...template.entries()].map(([cols, value]) => (
         <div key={cols} className="grid gap-4">
